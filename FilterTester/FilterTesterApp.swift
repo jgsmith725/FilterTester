6f050e2aa1ced76ca31115176ec3348b6bdef0d0
Jack Smith <jgsmith725@gmail.com>
2022-08-21 22:48:00 -0500

    Initial Commit

---
+++ FilterTester/FilterTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterTesterApp.swift
+//  FilterTester
+//
+//  Created by Jack Smith on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FilterTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
